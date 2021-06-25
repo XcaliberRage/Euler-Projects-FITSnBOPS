98055351cfada28e70648e584c7935ce7d8ce1b5
XcaliberRage <xcalibershard@gmail.com>
2021-06-25 14:22:46 +0200

    Git is confusing me

---
+++ target/debug/deps/FITSnBOPS.d
@@ -0,0 +1,5 @@
+C:\Users\Nathan\PycharmProjects\EulerProblems\FITSnBOPS\target\debug\deps\FITSnBOPS.exe: src\main.rs
+
+C:\Users\Nathan\PycharmProjects\EulerProblems\FITSnBOPS\target\debug\deps\FITSnBOPS.d: src\main.rs
+
+src\main.rs:
